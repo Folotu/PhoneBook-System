ff1694cc718b98cbdf8e240cb0629795e4436540
Frank Olotu <folotu@ucmerced.edu>
2021-12-23 01:37:37 -0800

    Add project files.

---
+++ PhoneBook System.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+class Phonebook
+{
+private:
+    int srno;
+    char name[25], mobile[15], email[30], group[20];
+
+public:
+    int getSrNo() { return srno; };
+    void storeData()
+    {
+
+    }
+
+
+void menu()
+{
+    int ch;
+
+    do
+    {
+        
+    }
+}
+
+int main()
+{
+    
+}
+
