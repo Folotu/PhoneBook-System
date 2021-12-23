df3b74af0876d010e085a9f3cadbee329ab4b5a9
Frank Olotu <folotu@ucmerced.edu>
2021-12-23 01:48:51 -0800

    Finished IO statements for storedata func

--- PhoneBook System.cpp
+++ PhoneBook System.cpp
@@ -15,6 +15,26 @@ public:
     int getSrNo() { return srno; };
     void storeData()
     {
+        cout << "\n..................CREATE NEW PHONE RECORD................\n";
+        cout << "Enter your serial number : ";
+        cin >> srno; cin.ignore();
+
+        cout << "Enter Record Name    : "; 
+        cin.getline(name, 25);
+        cout << "Enter Mobile Number : ";
+        cin.getline(mobile, 15);
+
+        cout << "Enter E-Mail address : ";
+        cin.getline(email, 30);
+
+        cout << "Enter Record Group : ";
+        cin.getline(group, 20);
+        cout << endl;
+    }
+
+    void showData()
+    {
+
 
     }
 
