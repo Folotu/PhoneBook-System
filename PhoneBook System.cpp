97dbdb04bc67084763e0975ea413b435fea027ba
Frank Olotu <folotu@ucmerced.edu>
2021-12-23 02:20:50 -0800

    Finished Search by serial num func

--- PhoneBook System.cpp
+++ PhoneBook System.cpp
@@ -4,6 +4,9 @@
 #include <conio.h>
 
 using namespace std;
+using std::cout;
+
+void SearchBySrNo();
 
 class Phonebook
 {
@@ -53,6 +56,45 @@ public:
         fout.write((char*)&b, sizeof(b));
         fout.close();
         cout << "\nRecord Saved to File.........\n";
+    }
+
+    void DisplayRecords()
+    {
+        ifstream fin;
+        fin.open("PhoneBook.dat", ios::out | ios::binary | ios::app);
+        while (fin.read((char*)&b, sizeof(b)))
+        {
+            b.showData();
+        }
+        fin.close();
+        cout << "\nReading of Data file completed..........\n";      
+    }
+
+    void SearchBySrNo();
+    {
+        ifstream fin;
+        int n, flag(0);
+
+        fin.open("PhoneBook.dat", ios::out | ios::binary | ios::app);
+        cout << "Enter Serical Number of Record to Display: ";
+        cin >> n;
+
+        while (fin.read((char*)&b, sizeof(b)))
+        {
+            if (n == b.getSrNo())
+            {
+                b.showData();
+                flag = flag + 1; 
+
+                cout << "\n\n........Record Found and Displayed...........\n";
+            }
+        }
+        fin.close();
+        if (flag == 0)
+        {
+            cout << "\n The Record of the Serial Number " << n << " is not in file.........\n";
+            cout << "\n Reading of Data File Completed..............\n";
+        }
     }
 
 
